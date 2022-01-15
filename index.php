b14eb8f914045bfc501da56cc31b6077e8f88505
u2t-saopao <97742494+u2t-saopao@users.noreply.github.com>
2022-01-15 18:22:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -168,7 +168,7 @@
                             <h4 id="date_time_now" style=" text-align: end; font-size: 17px; padding-top: 8%; padding-left: 13%;  right: 0; font-family: 'IBM Plex Sans Thai', sans-serif;"></h4>
                         </div>
                         <div class="row">
-                            <span style="font-size: 17px;margin-left: 15px; margin-top: 12px;text-align: center; right: 0; font-family: 'IBM Plex Sans Thai', sans-serif;">Last update:</span>
+                            <span style="font-size: 17px; margin-top: 12px;text-align: center; right: 0; font-family: 'IBM Plex Sans Thai', sans-serif;">Last update:</span>
                             <span id="date_time"
                              style="font-size: 17px; margin-top: 12px;text-align: end; right: 0; font-family: 'IBM Plex Sans Thai', sans-serif;">
                             </span>
